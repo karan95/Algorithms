a073c069b207c86a21a7b5defa9c4d9c06952b15
Karansinh Thakor <kmthakor95@gmail.com>
2016-12-30 14:27:47 +0530

    Update InsertionSort.java

--- Insertion Sort/InsertionSort.java
+++ Insertion Sort/InsertionSort.java
@@ -3,7 +3,7 @@
 *
 * @author  Karansinh Thakor
 */
-class InseritonSort {
+class InsertionSort {
     public static void main(String[] args){
         if(args.length == 0) {
             System.out.println("No argument supplied");
