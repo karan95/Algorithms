95c60e217571dc3f7eddf0b1e9ceb86e2a14b217
karansinh.thakor <Karansinh.Thakor@Sophos.com>
2017-04-10 19:34:40 +0530

    merge sort changes

--- Merge Sort/Merge.java
+++ Merge Sort/Merge.java
@@ -27,11 +27,22 @@ class Merge {
     private static boolean less(Comparable a, Comparable b) {
         return a.compareTo(b) < 0;
     }
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+    private static boolean isSorted(Comparable[] a, int low, int high) {
+        for (int i = low + 1; i <= high; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
     public static void main(String args[]) {
         String[] a = {"f","v","b","n","r","d","l","c","p","o","u","z"};
         Merge.sort(a);
         for(int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
+        boolean isSorted = Merge.isSorted(a);
+        System.out.println("isSorted:"+isSorted);
     }
 }
